2c1a2061736e4a883e8e80b2dd6212aa32490b57
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-09-08 08:16:39 +0530

    updated

--- students/views.py
+++ students/views.py
@@ -16,8 +16,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .serializers import StudentSerializer
-
-
+  
 # =========================================================
 # LOGIN
 # =========================================================
